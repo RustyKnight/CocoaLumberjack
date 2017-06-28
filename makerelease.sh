6feb984159e07dd514061401dcc497e588b40f72
Shane Whitehead <shane.whitehead@beamcommunications.com>
2017-06-28 17:42:19 +1000

    Xcode 9b2

--- makerelease.sh
+++ makerelease.sh
@@ -1,2 +1,6 @@
-carthage build --no-skip-current
-carthage archive CocoaLumberjack CocoaLumberjackSwift
+sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
+xcodebuild -version
+time carthage build --no-skip-current
+time carthage archive CocoaLumberjack CocoaLumberjackSwift
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+xcodebuild -version
