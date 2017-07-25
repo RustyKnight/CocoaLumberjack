3ed813c74f2edaf19aa05b3b1f3dc0a06d778565
Shane Whitehead <shane.whitehead@beamcommunications.com>
2017-07-26 09:25:32 +1000

    Update script

--- makerelease.sh
+++ makerelease.sh
@@ -1,5 +1,7 @@
 sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
 xcodebuild -version
+rm *.zip
+rm -rf Carthage
 time carthage build --no-skip-current
 time carthage archive CocoaLumberjack CocoaLumberjackSwift
 sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
